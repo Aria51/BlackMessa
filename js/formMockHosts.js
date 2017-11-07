45a7db3232f268eeb0187db5a109a5c9e7ba29a0
aria5 <aria51@yandex.ru>
2017-11-08 01:38:52 +0600

    быстрая замена статусов

--- js/formMockHosts.js
+++ js/formMockHosts.js
@@ -1,49 +1,53 @@
 function getMockHostList() {	
-			$('#hostList').empty();
-			var tt;
+	$('#hostList').empty();
+	var tt;
 
-			pingService();
-			$.each(hostMass, function () {
-				tt=this;
+	pingService();
+	$.each(hostMass, function () {
+		tt=this;
 				//tt.status=pingService(tt.url);
 				ter(tt.descriptions,tt.url,tt.statuss,tt.serviceName);					
 			}); 
-			
-			$.each(hostMass, function () {
+
+	$.each(hostMass, function () {
 
 
-			$.when(resp=$.getJSON(this.url + '/BlackMessa/pingService')
-						).then(function () {							
-									
-							}, function () {
-								
-							})
-			});
+		$.when(resp=$.getJSON(this.url + '/BlackMessa/pingService')
+			).then(function () {							
+
+			}, function () {
+
+			})
+		});
 };
 
 
-					
+
 function disableButton(source){
 	if(source!=true){ return 'disabled'}
 }
 
 function ter(_description, url, status, serviceName) {
 	$('#hostList').append(
-						'<tr class="users_row"><td>'
-						+_description
-						+'</td><td>'
-						+url
-						+'</td><td>'
-						+'<p id="'
-						+serviceName
-						+'">'
-						+status
-						+'</p>'						
-						+'</td><td>'
-						+ '<button class="button_u btn btn-info" id="' 
-						+ serviceName
-						+'">Выбрать</button>'
-						+'</td>');
+		'<tr class="users_row"><td>'
+		+_description
+		+'</td><td>'
+		+url
+		+'</td><td>'
+		+'<p id="'
+		+serviceName
+		+'">'
+		+ function () {
+			var t;
+			if (status==true){t= "Активен"}else{t= "Не активен"};
+			return t;
+		}()
+		+'</p>'						
+		+'</td><td>'
+		+ '<button class="button_u btn btn-info" id="' 
+		+ serviceName
+		+'">Выбрать</button>'
+		+'</td>');
 	if(status==false){$('button#'+serviceName).attr('disabled','disabled');}
 }
 
@@ -51,22 +55,22 @@ function pingService() {
 	//var resp = 	$.getJSON(hostUrl + '/BlackMessa/pingService');
 	//return resp;
 	$.each(hostMass, function () {
-				tt=this;
-				$.ajax({
-					url: tt.url + '/BlackMessa/pingService',					
-					async: false,
-					dataType: 'text',
-					type: 'GET',					
-					complete: function (e, xhr, settings) {
-						if(e.status==200){
-							tt.statuss=true
-						}
-						else
-						{
-							tt.statuss=false
-						}
-					}				
-				});
+		tt=this;
+		$.ajax({
+			url: tt.url + '/BlackMessa/pingService',					
+			async: false,
+			dataType: 'text',
+			type: 'GET',					
+			complete: function (e, xhr, settings) {
+				if(e.status==200){
+					tt.statuss=true
+				}
+				else
+				{
+					tt.statuss=false
+				}
+			}				
+		});
 
 	});
 	console.log(tt.statuss);
